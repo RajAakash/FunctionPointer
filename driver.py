f52cc7c66036cb7c65bb0a799b76e5fbe1eccf55
RajAakash <https://github.com/rajAakash01>
2023-10-26 19:52:43 -0500

    Small changes

--- driver.py
+++ driver.py
@@ -7,7 +7,7 @@ result2 = math_operation.subtract(10, 4)
 result3 = math_operation.multiply(6, 7)
 result4 = math_operation.divide(8, 2)
 
-print(f"Addition: {result1}")
+print(f"Addition is this : {result1}")
 print(f"Subtraction: {result2}")
 print(f"Multiplication: {result3}")
 print(f"Division: {result4}")
