62187e7d08adbe6f471d47503ab89aeed7737235
RajAakash <https://github.com/rajAakash01>
2023-10-26 19:46:37 -0500

    First commit for the job

---
+++ driver.py
@@ -0,0 +1,13 @@
+import math_operation
+
+
+# Use the functions from the imported module
+result1 = math_operation.add(5, 3)
+result2 = math_operation.subtract(10, 4)
+result3 = math_operation.multiply(6, 7)
+result4 = math_operation.divide(8, 2)
+
+print(f"Addition: {result1}")
+print(f"Subtraction: {result2}")
+print(f"Multiplication: {result3}")
+print(f"Division: {result4}")
